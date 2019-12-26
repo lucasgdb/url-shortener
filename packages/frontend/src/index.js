e304f1547d62df1bf980e6288ee6f4f9a6a855e6
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-12-26 17:14:03 -0300

    routes created and more

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './components/Input';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+const Home = lazy(() => import('./pages/Home'));
+const WrongPage = lazy(() => import('./pages/404'));
+
+ReactDOM.render(
+	<Suspense fallback={<div>Carregando...</div>}>
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact>
+					<Home />
+				</Route>
+
+				<Route path="*">
+					<WrongPage />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	</Suspense>,
+	document.getElementById('root')
+);
---
+++ packages/frontend/src/pages/404/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function WrongPage() {
+	return <div>Erro 404!</div>;
+}
