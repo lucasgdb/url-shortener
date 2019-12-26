1b833801fa95808e692f53830c51f5e62cd6fa01
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-12-26 16:18:26 -0300

    concurrently added; improvements

--- packages/frontend/src/components/Input/index.js
+++ packages/frontend/src/components/Input/index.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Client as Styletron } from 'styletron-engine-atomic';
 import { Provider as StyletronProvider } from 'styletron-react';
-import { LightTheme, BaseProvider, styled } from 'baseui';
+import { LightTheme, DarkTheme, BaseProvider, styled } from 'baseui';
 import { StatefulInput } from 'baseui/input';
+import { Button } from 'baseui/button';
 
 const engine = new Styletron();
 
 const Centered = styled('div', {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	height: '100%',
 });
 
 export default function Hello() {
-    return (
-        <StyletronProvider value={engine}>
-            <BaseProvider theme={LightTheme}>
-                <Centered>
-                    <StatefulInput />
-                </Centered>
-            </BaseProvider>
-        </StyletronProvider>
-    );
+	const [theme, setTheme] = useState(false);
+
+	return (
+		<StyletronProvider value={engine}>
+			<BaseProvider theme={theme ? LightTheme : DarkTheme}>
+				<Centered>
+					<StatefulInput />
+				</Centered>
+
+				<Button
+					style={{ marginTop: 20 }}
+					onClick={() => setTheme(!theme)}
+				>
+					Mudar tema
+				</Button>
+			</BaseProvider>
+		</StyletronProvider>
+	);
 }
