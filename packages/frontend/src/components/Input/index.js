cdc7724f56a007de298a4b4fa91a5076e6a5461c
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-12-26 01:24:37 -0300

    yarn workspaces and prettier + eslint were configured

---
+++ packages/frontend/src/components/Input/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider, styled } from 'baseui';
+import { StatefulInput } from 'baseui/input';
+
+const engine = new Styletron();
+
+const Centered = styled('div', {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+});
+
+export default function Hello() {
+    return (
+        <StyletronProvider value={engine}>
+            <BaseProvider theme={LightTheme}>
+                <Centered>
+                    <StatefulInput />
+                </Centered>
+            </BaseProvider>
+        </StyletronProvider>
+    );
+}
