538a814b7a469fcd76efd74c3a8dc1f408fbd323
Lucas Bittencourt <lucasnaja0@gmail.com>
2019-12-27 15:02:11 -0300

    improvements

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/url-shortener', {
+mongoose.connect('mongodb://localhost:27017/url-shortener', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 mongoose.connection.once('open', () => console.log('mongodb connected!'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/', (req, res) => res.status(200).json({ status: true }));
+app.use(cors());
+app.use('/api', (req, res) => require('./routes'));
 
 app.listen(3001);
